0a133fdc7667e1fd5b39aa216ed0ffdbc769a059
Iberi Daniel <danieliberi99@gmail.com>
2022-08-15 18:27:26 +0100

    created home page. setting up navigation and APIs

---
+++ screens/MapScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>MapScreen and all</Text>
+    </View>
+  )
+}
+
+export default MapScreen
---
+++ store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import navReducer from './slices/navSlice';
+
+export default store = configureStore({
+    reducer: {
+        nav: navReducer,
+    },
+});
