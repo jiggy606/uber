26f636475f320ee7415b80ae6fda0c87d738d2bd
Iberi Daniel <danieliberi99@gmail.com>
2022-08-16 17:54:15 +0100

    HomeScreen done. Pls review

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,14 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'; 
+import { useDispatch } from 'react-redux/es/hooks/useDispatch';
+import { setDestination, setOrigin } from '../slices/navSlice';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -12,6 +18,35 @@ const HomeScreen = () => {
                     source={{
                     uri: 'https://links.papareact.com/gzs',
                     }} />
+                
+                <GooglePlacesAutocomplete
+                    placeholder='Where from'
+                    nearbyPlacesAPI='GooglePlacesSearch'
+                    debounce={400}
+                    minLength={3}
+                    enablePoweredByContainer={false}
+                    returnKeyType={'search'}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en'
+                    }}
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        }
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description,
+                        }));
+
+                        dispatch(setDestination(null));
+                    }}
+                />
                 
                 <NavOptions />
             </View>
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,15 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import tw from 'tailwind-react-native-classnames';
 
 const MapScreen = () => {
   return (
     <View>
       <Text>MapScreen and all</Text>
+
+      <View style={tw`h-1/2 bg-red-300`}></View>
+
+      <View style={tw`h-1/2 bg-red-900`}></View>
     </View>
   )
 }
